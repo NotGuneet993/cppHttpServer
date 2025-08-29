161c6ec70b004f3c2f463941026bf503ed3809b7
Guneet <guberoi@icloud.com>
2025-08-29 18:36:30 -0400

    TCP header done

--- tcp.h
+++ tcp.h
@@ -1,5 +1,28 @@
 #ifndef TCP_H
-#define TCH_H
+#define TCP_H
 
+#include <iostream>
+#include <netinet/in.h>
+
+class TCP {
+public:
+
+    TCP(const std::string& ip, int port);
+    ~TCP();
+
+    TCP(const TCP&) = delete;
+    TCP& operator=(const TCP&) = delete;
+
+    bool bindAndListen(int backlog);
+    void serve(class IConnectionHandler& connectionHandler);
+
+    ssize_t receive(int fd, void* buffer, size_t len);
+    bool sendAll(int fd, const void* data, size_t len);
+
+private:
+    
+    int  socketFileDescriptor;
+    sockaddr_in address;
+};
 
 #endif
